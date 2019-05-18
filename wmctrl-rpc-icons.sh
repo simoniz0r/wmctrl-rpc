1e2c73b4e59ac343acd3bcdfcc8836e038ba9661
simoniz0r <bnu.fantasma@gmail.com>
2019-05-18 01:14:54 -0500

    Add list of icons to be sourced on run

---
+++ wmctrl-rpc-icons.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Title: wmctrl-rpc-icons.sh
+# Author: simonizor
+# Description: Contains list of valid icons for wmctrl-rpc
+# License: MIT
+
+ERP_ICON_LIST=("code" "chrome" "falkon" "firefox" "generic-window" "kitty" "mpv" "opensuse" "tux")
